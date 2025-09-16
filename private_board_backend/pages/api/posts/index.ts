1e88305e2f81718fc33d1156e958253fd1b9ae11
VICTOR <jeonseongcheol@naver.com>
2025-09-16 11:17:56 +0900

    Use Prisma singleton in Next.js API routes

--- private_board_backend/pages/api/posts/index.ts
+++ private_board_backend/pages/api/posts/index.ts
@@ -1,8 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { PrismaClient } from '@prisma/client'
 import { getTokenFromReq, verifyJwt } from '@/lib/auth'
-
-const prisma = new PrismaClient()
+import prisma from '@/lib/prisma'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
